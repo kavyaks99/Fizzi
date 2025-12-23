6c352c710ad5319f24fa40020ba52a93036e073b
kavyaks99 <kskavya3661@gmail.com>
2025-12-23 12:50:14 +0530

    Added loader

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -2,28 +2,40 @@
 
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader),
+  { ssr: false }
+);
 
 type Props = {};
 
 function ViewCanvas({}: Props) {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        overflow: "hidden",
-        zIndex: 30,
-        pointerEvents: "none",
-      }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-      camera={{ fov: 30 }}
-    >
-      <View.Port />
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          zIndex: 30,
+          pointerEvents: "none",
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+        camera={{ fov: 30 }}
+      >
+        <Suspense fallback={null}>
+          <View.Port />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
 
