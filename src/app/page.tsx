c1713686641dea400600e31e4f9267f46a464ce1
kavyaks99 <kskavya3661@gmail.com>
2025-12-24 12:06:56 +0530

    Implemented rotationg can animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Bounded } from "../components/Bounded";
 import Home from "./pages/Home";
 import SkyDive from "./pages/skyDive/SkyDive";
+import Carousel from "./pages/carousel/Carousel";
 
 export default function Main() {
   return (
     <>
       <Home />
       <SkyDive />
+      <Carousel />
     </>
   );
 }
